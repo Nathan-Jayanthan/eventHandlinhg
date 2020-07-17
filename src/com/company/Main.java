61ad63911f674580ffd0b4dfce654622fe7663d9
nathan-jayanthan <nathan.jayanthan@yahoo.com>
2020-07-16 20:04:12 -0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+import  javax.swing.JFrame;
+
+public class Main {
+
+    public static void main(String[] args) {
+        tuna bucky = new tuna();
+        bucky.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        bucky.setSize(350, 100);
+        bucky.setVisible(true);
+
+
+    }
+}
